95054cc38c960fc1b6a4fb49ae291d10ed238588
Fred K. Schott <fkschott@gmail.com>
2020-07-22 22:23:19 -0700

    dont run esbuild on .js files

--- test/build/linked-pkg/expected-build/_dist_/index.js
+++ test/build/linked-pkg/expected-build/_dist_/index.js
@@ -1,6 +1,7 @@
-import link from "/web_modules/@snowpack/test-link.js";
-import deeplink from "/web_modules/@snowpack/test-link/deep.js";
-import local from "/web_modules/@snowpack/test-local.js";
+import link from '/web_modules/@snowpack/test-link.js';
+import deeplink from '/web_modules/@snowpack/test-link/deep.js';
+import local from '/web_modules/@snowpack/test-local.js';
+
 link();
 deeplink();
 local();
--- test/build/yarn-workspaces/packages/snowpack/expected-build/_dist_/index.js
+++ test/build/yarn-workspaces/packages/snowpack/expected-build/_dist_/index.js
@@ -1,2 +1,3 @@
-import {makeFlat} from "/web_modules/yarn-workspace-utils.js";
+import {makeFlat} from '/web_modules/yarn-workspace-utils.js';
+
 console.log(makeFlat([1, [2, [3, [4, [5], 6], 7], 8], 9]));
